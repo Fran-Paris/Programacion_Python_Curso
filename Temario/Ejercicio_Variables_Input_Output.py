ae34b0d07a3826d617323f98f999ccdfc589a73d
Fran-Paris <paris.franciscojose@gmail.com>
2023-01-09 17:32:30 +0100

    nuevo

---
+++ Temario/Ejercicio_Variables_Input_Output.py
@@ -0,0 +1,25 @@
+numero1= input("Introduzca el primer numero: ")
+numero2= input("Introduzca el segundo numero: ")
+numero1=int(numero1)
+numero2=int(numero2)
+print(numero1,numero2)
+operacion= "multiplicación"
+print(operacion)
+operacion = numero1*numero2
+print(operacion)
+operacion= "suma"
+print(operacion)
+operacion = numero1+numero2
+print(operacion)
+operacion= "división"
+print(operacion)
+operacion = numero1/numero2
+print(operacion)
+operacion= "resta"
+print(operacion)
+operacion = numero1-numero2
+print(operacion)
+operacion= "potencia"
+print(operacion)
+operacion = numero1**numero2
+print(operacion)

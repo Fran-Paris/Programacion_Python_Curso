73d11908f0f257b1e1436ccd209033e0ad62fcf1
Fran-Paris <paris.franciscojose@gmail.com>
2023-01-09 15:19:03 +0100

    starting

--- Temario/Hola_Mundo.py
+++ Temario/Hola_Mundo.py
@@ -5,3 +5,10 @@ print("hola ")
 
 
 print(input("cuantos años tienes?: "))
+
+
+#Hacer una pausa en la ejecución
+print("Era una tarde lluviosa de abril")
+print("Tras varias semanas lloviendo")
+input("...Presiona intro para continuar")
+print("nuestro protragonista continuó")
